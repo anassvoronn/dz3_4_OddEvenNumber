b5b1dc0712271fbadbb4f9a3cb5ec9cc7dc16103
anassvoronn <UQzbwQCt7R3uMfz>
2022-08-08 16:25:49 +0300

    EvenNumberChecker

--- src/main/java/org/dz/solution/EvenNumberChecker.java
+++ src/main/java/org/dz/solution/EvenNumberChecker.java
@@ -1,14 +1,12 @@
 package org.dz.solution;
 
 public class EvenNumberChecker {
-    public boolean check(final int number){
-        for (int i = 1; i < 10; i++){
-            if (i % 2 == 0){
-                System.out.println("Even " + i);
-            }else {
-                System.out.println("Odd " + i);
-            }
-        }
-        return false;
+    public boolean check(final int number) {
+
+
+        return number % 2 == 0;
+
+
     }
 }
+
--- src/test/java/org/dz/solution/EvenNumberCheckerTest.java
+++ src/test/java/org/dz/solution/EvenNumberCheckerTest.java
@@ -7,9 +7,45 @@ import org.junit.Test;
 public class EvenNumberCheckerTest {
 
     @Test
-    public void check() {
+    public void check5() {
         EvenNumberChecker checker = new EvenNumberChecker();
         boolean result = checker.check(5);
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void check0() {
+        EvenNumberChecker checker = new EvenNumberChecker();
+        boolean result = checker.check(0);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void check13() {
+        EvenNumberChecker checker = new EvenNumberChecker();
+        boolean result = checker.check(13);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void check2() {
+        EvenNumberChecker checker = new EvenNumberChecker();
+        boolean result = checker.check(2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void check1000() {
+        EvenNumberChecker checker = new EvenNumberChecker();
+        boolean result = checker.check(1000);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void checkMinus1001() {
+        EvenNumberChecker checker = new EvenNumberChecker();
+        boolean result = checker.check(-1001);
+        Assert.assertFalse(result);
+    }
+
 }
